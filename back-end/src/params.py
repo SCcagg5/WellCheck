3d394c65ff639f30a88e09d37ff22ec4cb35a953
SCcagg5 <sccagg5@gmail.com>
2019-05-19 12:55:14 +0200

    singleton comp fix

--- back-end/src/params.py
+++ back-end/src/params.py
@@ -2,11 +2,11 @@ from bottle import request
 
 class check:
     def contain(json, array):
-        if json == None:
+        if json is None:
             return [False, "Invalid json received ", 400]
         for i in array:
             try:
-                if json[i] == None:
+                if json[i] is None:
                     return [False, "Null parameter : " + i, 400]
             except:
                 return [False, "Missing parameter : " + i, 400]
