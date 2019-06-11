8ded8ffd82f39060679f8fa5b9dc5d5f0dc4d5d4
SCcagg5 <sccagg5@gmail.com>
2019-06-11 17:33:06 +0200

    adding private

--- back-end/src/Controller/routesfunc.py
+++ back-end/src/Controller/routesfunc.py
@@ -12,7 +12,7 @@ def connect(cn, nextc):
 
     use = user(cn.pr["mail"], cn.pr["password"], cn.pr["token"])
     err = use.connect()
-    cn.pr["user_id"] = use.id
+    cn.private["user_id"] = use.id
     return cn.call_next(nextc, err)
 
 def register(cn, nextc):
@@ -29,7 +29,7 @@ def addpoint(cn, nextc):
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
     cn.pr = err[1]
-    device = point(None, cn.pr["user_id"], cn.pr["key"], cn.pr["sig_id"])
+    device = point(None, cn.private["user_id"], cn.pr["key"], cn.pr["sig_id"])
     err = device.infos()
     return cn.call_next(nextc, err)
 
@@ -37,7 +37,7 @@ def infos(cn, nextc):
     err = check.contain(cn.pr, ["point_id"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
-    device = point(cn.pr["point_id"], cn.pr["user_id"])
+    device = point(cn.pr["point_id"], cn.private["user_id"])
     err = device.infos()
     return cn.call_next(nextc, err)
 
@@ -45,7 +45,7 @@ def share(cn, nextc):
     err = check.contain(cn.pr, ["point_id", "mail_to"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
-    device = point(cn.pr["point_id"], cn.pr["user_id"])
+    device = point(cn.pr["point_id"], cn.private["user_id"])
     err = device.share(cn.pr["mail_to"])
     return cn.call_next(nextc, err)
 
@@ -53,16 +53,16 @@ def surname(cn, nextc):
     err = check.contain(cn.pr, ["point_id", "surname"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
-    device = point(cn.pr["point_id"], cn.pr["user_id"])
+    device = point(cn.pr["point_id"], cn.private["user_id"])
     err = device.rename(cn.pr["surname"])
     return cn.call_next(nextc, err)
 
 def getall(cn, nextc):
-    devices = points(cn.pr["user_id"])
+    devices = points(cn.private["user_id"])
     err = devices.getall()
     return cn.call_next(nextc, err)
 
 def getalldetails(cn, nextc):
-    devices = points(cn.pr["user_id"])
+    devices = points(cn.private["user_id"])
     err = devices.getalldetails()
     return cn.call_next(nextc, err)
